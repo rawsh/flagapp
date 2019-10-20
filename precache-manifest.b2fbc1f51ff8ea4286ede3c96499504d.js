1f875af749a07ae966863cf1e408a8f4a601d22d
Robert Washbourne <rawashbourne@gmail.com>
2019-10-20 11:34:00 -0400

    fr

---
+++ precache-manifest.b2fbc1f51ff8ea4286ede3c96499504d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e965c62cf5b2038c98f049b5e2b61f15",
+    "url": "/flagapp/index.html"
+  },
+  {
+    "revision": "b47d8e4bb375db3ccdd6",
+    "url": "/flagapp/static/css/main.d6842306.chunk.css"
+  },
+  {
+    "revision": "2be093505ff03bad0ce5",
+    "url": "/flagapp/static/js/2.ff5e1585.chunk.js"
+  },
+  {
+    "revision": "b47d8e4bb375db3ccdd6",
+    "url": "/flagapp/static/js/main.40c14475.chunk.js"
+  },
+  {
+    "revision": "73afe01ad5f393bd60b1",
+    "url": "/flagapp/static/js/runtime-main.246123a6.js"
+  }
+]);
